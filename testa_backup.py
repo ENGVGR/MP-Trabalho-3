6249f6748b3845cebc92ac99b62990606dc6fa6b
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-08-10 12:17:33 -0300

    Implementado teste para comparar as datas dos arquivos

--- testa_backup.py
+++ testa_backup.py
@@ -6,6 +6,7 @@ import os
 import shutil
 from backup import arquivos_presentes
 from backup import datas_dos_arquivos
+from backup import compara_datas
 
 def teste_arquivos_presentes():
   """! Testa se existe uma lista de arquivos x's na pasta y.
@@ -68,3 +69,18 @@ def teste_datas_dos_arquivos():
   assert datas_dos_arquivos(pasta_teste, arquivos_teste[2:3]) == datas[2:3]
 
   assert datas_dos_arquivos(pasta_teste, arquivos_teste[0:1]) == datas[0:1]
+
+def teste_compara_datas():
+  datas_pasta1 = [[2022,8,10,11,30,10], [2022,8,10,11,30,15],
+  [2022,8,10,11,30,21], [2022,8,10,10,30,26]]
+
+  datas_pasta2 = [[2022,8,10,11,30,10], [2023,8,10,11,30,15],
+  [2022,8,10,11,30,20], [2022,8,10,11,40,26]]
+
+  #Opções de saída:
+  saida1 = "Primeiro é mais recente"
+  saida2 = "Segundo é mais recente"
+  saida3 = "As datas são iguais"
+
+  assert compara_datas(datas_pasta1, datas_pasta2) == [saida3, saida2,
+  saida1, saida2]
