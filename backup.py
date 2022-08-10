2e5d649957160c5a553a7f219e88d24f080594e2
Vinícius Ribeiro <vgrgonzaga@outlook.com>
2022-08-10 12:40:41 -0300

    Implementado a funcao que retorna a comparacao das datas dos arquivos de duas pastas

--- backup.py
+++ backup.py
@@ -27,6 +27,67 @@ def datas_dos_arquivos(pasta, arquivos):
   datas = []
   for arquivo in arquivos:
     data = os.path.getmtime(f"{pasta}/{arquivo}")
-    year,month,day,hour,minute,second=time.localtime(data)[:-3]
-    datas.append([year, month, day, hour, minute, second])
+    ano,mes,dia,hora,minuto,segundo=time.localtime(data)[:-3]
+    datas.append([ano, mes, dia, hora, minuto, segundo])
   return datas
+
+def compara_datas(datas1, datas2):
+  """! Função que compara as datas de duas pastas.
+  @param datas1  Datas da primeira pasta, no formato [ano,mes,dia,hora,minuto,segundo].
+  @param datas2  Datas da segunda pasta, no formato [ano,mes,dia,hora,minuto,segundo].
+  @return  Retorna uma lista com o resultado das comparações de datas.
+  """
+  comparacoes = []
+
+  for i in range(len(datas1)):
+    ano1 = datas1[i][0]
+    ano2 = datas2[i][0]
+    mes1 = datas1[i][1]
+    mes2 = datas2[i][1]
+    dia1 = datas1[i][2]
+    dia2 = datas2[i][2]
+    hora1 = datas1[i][3]
+    hora2 = datas2[i][3]
+    minuto1 = datas1[i][4]
+    minuto2 = datas2[i][4]
+    segundo1 = datas1[i][5]
+    segundo2 = datas2[i][5]
+
+    if ano1 != ano2:
+      if ano1 > ano2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+
+    elif mes1 != mes2:
+      if mes1 > mes2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+
+    elif dia1 != dia2:
+      if dia1 > dia2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+
+    elif hora1 != hora2:
+      if hora1 > hora2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+
+    elif minuto1 != minuto2:
+      if minuto1 > minuto2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+
+    elif segundo1 != segundo2:
+      if segundo1 > segundo2:
+        comparacoes.append("Primeiro é mais recente")
+      else:
+        comparacoes.append("Segundo é mais recente")
+    else:
+      comparacoes.append("As datas são iguais")
+  return comparacoes
